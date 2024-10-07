53aa57f4b200958925b0821484174a3f959cf834
Walter Aoiama Nagai <walternagai@unifei.edu.br>
2024-10-07 00:12:19 -0300

    Atualizar função de comparação de arquivos e análise de similaridade

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def generate_response_groq(api_key, model, file_content, file_content_to_compare
                      max_tokens=1024,
                      api_key=api_key)
     
-    prompt = f"Compare o código: ```{file_content}``` com o código: ```{file_content_to_compare}``` e identifique trechos plagiados."
+    prompt = f"Compare o código 1: ```{file_content}``` com o código 2: ```{file_content_to_compare}``` e identifique trechos plagiados."
 
     messages = [
         ("system", 
@@ -166,12 +166,22 @@ def stream_data(response):
         time.sleep(0.01)
 
 def id_generator(size=16, chars=string.ascii_lowercase + string.digits):
+    """
+    Gera um identificador aleatório.
+    Parâmetros:
+    - size: int - Tamanho do identificador.
+    - chars: str - Caracteres possíveis para o identificador.
+    Retorno:
+    - str - Identificador gerado.
+    """
     return ''.join(random.choice(chars) for _ in range(size))
 
 def main():    
+    # Chaves de API e modelo Groq
     api_key = st.secrets.pytheo_groq.GROQ_API_KEY
     model = st.secrets.pytheo_groq.GROQ_MODEL
 
+    # Dicionário de linguagens de programação e extensões de arquivos
     dict_languages_extensions = {
         "C": "c",
         "C++": "cpp",
@@ -195,12 +205,14 @@ def main():
 
     columns_options = st.columns(2)
 
+    # Seleção da linguagem de programação
     with columns_options[0]:
         language_selected = st.selectbox("Escolha a linguagem de programação", 
                                          dict_languages_extensions.keys(), 
                                          help="Selecione a linguagem de programação dos arquivos que deseja comparar.")
         extension_file = dict_languages_extensions[language_selected].lower()
-       
+    
+    # Limite de similaridade entre os arquivos
     with columns_options[1]:
         limit = st.slider("Similaridade", 
                           min_value=0.0, 
@@ -209,51 +221,78 @@ def main():
                           step=0.05,
                           help="Selecione o limite de similaridade entre os arquivos para ser considerado plágio.")
 
+    # Carregamento do arquivo ZIP com os arquivos a serem comparados
     zip_file = st.file_uploader("Carregar arquivo ZIP", 
                                 type="zip",
                                 help="Carregue um arquivo ZIP contendo os arquivos a serem comparados.")
 
+    # se o diretório de extração não existir, cria o diretório
     if not os.path.exists(extract_path):
         os.makedirs(extract_path)
 
+    # se o arquivo ZIP foi carregado
     if zip_file:
+        # extrair os arquivos do arquivo ZIP
         with ZipFile(zip_file, 'r') as zip_ref:
             zip_ref.extractall(path=extract_path)
 
+        # obter a lista de arquivos extraídos
         files_path = os.path.join(extract_path, zip_file.name.replace(".zip", f""))
+
+        # obter a lista de arquivos extraídos
         files = os.listdir(files_path)
 
+        # se os arquivos extraídos possuem a extensão correta 
         if files[0].endswith(extension_file):
+            # exibir mensagem de sucesso
             st.toast(f"Arquivos extraídos com sucesso!")
+
+            # ler o conteúdo dos arquivos
             files_content = []
             for file in files:
-                file_path = os.path.join(files_path, file)                
-                file_stream = io.BytesIO(open(file_path, "rb").read())
+                file_stream = io.BytesIO(open(os.path.join(files_path, file), "rb").read())
                 files_content.append(read_file(file_stream))
 
             st.markdown("### Análise de similaridade entre os arquivos")
 
+            # gerar matriz de similaridades
             similarities_matrix = []
             for i in range(len(files_content)):
                 for j in range(i+1, len(files_content)):
+                    # comparar os arquivos e calcular a similaridade entre eles
                     similarity = comparate_files(files_content[i], files_content[j], language_selected)
+                    # adicionar à matriz de similaridades
                     similarities_matrix.append((files[i], files[j], similarity))
 
-            # ordenar a matriz de similaridades pela similaridade
+            # ordenar a matriz de similaridades pela maior similaridade decrescente
             similarities_matrix = sorted(similarities_matrix, key=lambda x: x[2], reverse=True)
+
+            # criar um DataFrame com a matriz de similaridades
             similarities_df = pd.DataFrame(similarities_matrix, columns=["Arquivo 1", "Arquivo 2", "Similaridade"])
+
+            # filtrar as similaridades acima do limite
             similarities_df_filtered = similarities_df[similarities_df["Similaridade"] > limit]
+
+            # se a matriz de similaridades filtrada não estiver vazia
             if not similarities_df_filtered.empty:
+                # exibir similaridades acima do limite
                 with st.status("Analisando similaridades...") as status:
+                    # adicionar coluna para armazenar a análise de similaridade entre os arquivos
                     for i, row in similarities_df_filtered.iterrows():
                         similarities_df_filtered.loc[i, "Analise"] = generate_response_groq(api_key, model, files_content[i], files_content[j])
                         status.update(label=f"Analisando similaridades... {i+1}/{len(similarities_df_filtered)}", state="running")
+
+                    # atualizar o status da análise
                     status.update(label="Análise finalizada!", state="complete")
 
+                # exibir similaridades acima do limite com a análise de similaridade entre os arquivos
                 for i, row in similarities_df_filtered.iterrows():
+                    # exibir similaridades acima do limite com a análise de similaridade entre os arquivos
                     st.markdown(f"#### {row['Arquivo 1']} x {row['Arquivo 2']} com {row['Similaridade']:.2%} de similaridade")
+                    # exibir análise de similaridade entre os arquivos em um stream de dados
                     st.write_stream(stream_data(row["Analise"]))
 
+                # exibir mensagem de conclusão
                 st.toast("Análise concluída!")
             else:
                 st.markdown("**Não foram encontrados trechos de código plagiados.**")
